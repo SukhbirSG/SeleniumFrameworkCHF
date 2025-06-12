34a4700e93e3dc19b3d8af658c5a8d60bcc35e7d
HP <sukhveergill18@gmail.com>
2025-06-12 16:58:08 +0530

    12 June

--- src/test/java/com/sukhbirsingh/SampleTests/Sample/Sample.java
+++ src/test/java/com/sukhbirsingh/SampleTests/Sample/Sample.java
@@ -1,4 +1,4 @@
-package com.sukhbirsingh.SampleTests;
+package com.sukhbirsingh.SampleTests.Sample;
 
 public class Sample {
     public static void main(String[] args) {
---
+++ src/test/java/com/sukhbirsingh/SampleTests/Sample/SampleTestNG.java
@@ -0,0 +1,18 @@
+package com.sukhbirsingh.SampleTests.Sample;
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTestNG {
+    @Description("Dry Run")
+    @Test
+    public void Test_Sample(){
+        Assert.assertTrue(true);
+    }
+    @Description("Dry Run 1")
+    @Test
+    public void Test_Sample1(){
+        Assert.assertTrue(true);
+    }
+}
